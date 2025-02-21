d01092ed7dc911d3f9c37c5ded2e1942e22d2eba
Chris Blyth <blythmeister@gmail.com>
2025-02-21 13:25:26 +0000

    Bump version to 2.0.0

--- src/TeamPasswordManagerClient/Properties/AssemblyInfo.cs
+++ src/TeamPasswordManagerClient/Properties/AssemblyInfo.cs
@@ -29,6 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.1.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
