1ce06d16c16c11f9ea877bbcb687b99beb8df90d
Chris Blyth <blythmeister@gmail.com>
2025-02-21 13:26:05 +0000

    Add test harness app

---
+++ src/TeamPasswordManagerClientTestHarness/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using TeamPasswordManagerClient;
+
+namespace TeamPasswordManagerClientTestHarness
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new TpmClient(new TpmConfig(args[0], args[1], args[2], args[3]));
+            var passwords = await client.Passwords.ListPasswords(611);
+            foreach (var password in passwords)
+            {
+                var value = await client.Passwords.GetPassword(password.Id);
+                Console.WriteLine($"{value.Name} - User: {value.Username} / Value: {value.Password}");
+            }
+
+            Console.WriteLine("Done...");
+            Console.ReadLine();
+        }
+    }
+}
